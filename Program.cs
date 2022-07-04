4a0ba7981d7d4548dc06b12a1ff3260c6e52ce48
jnanong <john.nanong@gmail.com>
2022-07-04 17:56:56 +0800

    .NET ToDoApi tutorial
    This is done locally. Realize that I need to push this in my GitHub.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+// builder.Services.AddSwaggerGen(c => {
+//     c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1"});
+// });
+
+// JohnN : Pre-created calls.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+}
+
+// Needed for Javascript response?
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
